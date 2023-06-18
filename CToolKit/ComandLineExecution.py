41dc949e15a77f7b77c69cadb5ce11f5620ba259
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-06-18 17:00:01 -0300

    at

--- CToolKit/ComandLineExecution.py
+++ CToolKit/ComandLineExecution.py
@@ -3,8 +3,13 @@ import subprocess
 
 
 class ComandLineExecution:
-
     def __init__(self, command: str):
+        """Execute the given comands
+        Args:
+            command (str):the comand to execute
+        Raises:
+            ExecutionError: if happen some execution error
+        """
         self.status_code, self.output = subprocess.getstatusoutput(command)
 
         if self.status_code != 0:
