f0fb3e06723e8929a48f67bfd9fef7ce120fd351
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-07-09 23:42:25 -0400

    at

--- CToolKit/FolderTestPreset/Execution.py
+++ CToolKit/FolderTestPreset/Execution.py
@@ -4,9 +4,11 @@ from os.path import  isdir
 from typing import List
 from ..comand_line_functions import execute_test_for_file
 from ..Errors.NotExpectedResult import NotExpectedResult
+from ..Errors.SideEffectFolderDiferent import SideEffectFolderDiferent
 from .output_formatation import  sanitize_value
 from ..ComandLineExecution import ComandLineExecution
 from .Creation import FolderTestPressetCreation
+from .folder_hash import are_folders_equal
 class FolderTestPressetExecution(FolderTestPressetCreation):
 
 
@@ -19,17 +21,37 @@ class FolderTestPressetExecution(FolderTestPressetCreation):
         if expected_file is None:
             raise FileNotFoundError(f'could not locate an expected.* in {folder}')
 
+
         with open(expected_file,'r') as arq:
             expected_content = arq.read()
 
         sanitized_expected :dict or List[str] = sanitize_value(expected_file,expected_content)
 
-        generated_result:dict or ComandLineExecution = execute_test_for_file(
-            file=execution_file,
-            compiler=self._compiler,
-            use_valgrind=self._use_valgrind,
-            raise_warnings=self._raise_warnings
-        )
+        try:
+            generated_result:dict or ComandLineExecution = execute_test_for_file(
+                file=execution_file,
+                compiler=self._compiler,
+                use_valgrind=self._use_valgrind,
+                raise_warnings=self._raise_warnings
+            )
+        except Exception as e:
+            if self._side_effect_folder_changed():
+                self._rebase_side_effect_folder()
+            raise e
+
+        #verifying it there is an side effect folder
+        side_effect_test = f'{folder}/side_effect'
+        if isdir(side_effect_test):
+            are_equal = are_folders_equal(side_effect_test,self._side_effect_folder)
+            if are_equal == False:
+                self._rebase_side_effect_folder()
+                raise SideEffectFolderDiferent(side_effect_test)
+
+        else:
+            if self._side_effect_folder_changed():
+                raise SideEffectFolderDiferent('there is no side effect folder passed')
+
+        
         if isinstance(generated_result,ComandLineExecution):
             output = generated_result.output
         else:
@@ -39,8 +61,11 @@ class FolderTestPressetExecution(FolderTestPressetCreation):
         sanitized_result = sanitize_value(expected_file,output)
 
 
+
+
         if sanitized_expected != sanitized_result:
             raise NotExpectedResult(sanitized_expected,sanitized_result)
+
 
 
 
@@ -54,7 +79,7 @@ class FolderTestPressetExecution(FolderTestPressetCreation):
 
             if isdir(path):
 
-                if e.startswith('R_') or e.startswith('WR_'):
+                if e.startswith('T_'):
                     try:
                         self._execute_test_presset(path)
                         self._print_if_setted_to_print_test(e, True)
@@ -63,22 +88,6 @@ class FolderTestPressetExecution(FolderTestPressetCreation):
                         raise ex
                 else:
                     self._execute_loop_test(path)
-
-            else:
-                try:
-                    if e.endswith('c') or e.endswith('cpp'):
-                        execute_test_for_file(
-                            file=path,
-                            compiler=self._compiler,
-                            use_valgrind=self._use_valgrind,
-                            raise_warnings=self._use_valgrind
-                        )
-                    self._print_if_setted_to_print_test(e, True)
-
-
-                except Exception as ex:
-                    self._print_if_setted_to_print_test(e, False)
-                    raise ex
 
 
 
